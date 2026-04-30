acd9b6d4820989a46aa35d0698388651f7765e7c
Pranav Sharma <pranav.sharma@rtctek.global>
2026-04-30 15:01:44 +0530

    Added a method for screenshot

--- src/test/java/com/example/utils/BaseTest.java
+++ src/test/java/com/example/utils/BaseTest.java
@@ -1,8 +1,12 @@
 package com.example.utils;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+
+import io.qameta.allure.Attachment;
 
 public class BaseTest {
     protected WebDriver driver;
@@ -12,6 +16,14 @@ public class BaseTest {
     public void setUp() throws Exception {
         driver = BrowserFactory.initDriver();
         driver.get(configReader.getBaseURL());
+       
+
+        attachScreenshot("Base URL Loaded Successfully");
+    }
+    
+    @Attachment(value = "{stepName}", type = "image/png")
+    public byte[] attachScreenshot(String stepName) {
+        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
     }
 
     @AfterClass
